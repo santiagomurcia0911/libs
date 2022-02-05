6a203b16b9da98c21f33cbb0b3f9cc231c4b0057
spena-mu <spena-mu@student.42barcelona.com>
2022-02-05 19:04:43 +0100

    strncmp

--- s_strncmp.c
+++ s_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: spena-mu <spena-mu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/05 18:19:38 by spena-mu          #+#    #+#             */
-/*   Updated: 2022/02/05 18:59:30 by spena-mu         ###   ########.fr       */
+/*   Updated: 2022/02/05 19:04:12 by spena-mu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,8 @@
 
 int	s_strncmp(const char *s1, const char *s2, size_t n)
 {
-	unsigned int i1;
-	int i2;
+	unsigned int	i1;
+	int				i2;
 
 	i1 = 0;
 	i2 = 0;
@@ -24,7 +24,7 @@ int	s_strncmp(const char *s1, const char *s2, size_t n)
 		i2 = (unsigned char)s1[i1] - (unsigned char)s2[i1];
 		i1++;
 	}
-	if ( i1 < n && !i2 && (s1[i1] == '\0' || s2[i1] == '\0'))
+	if (i1 < n && !i2 && (s1[i1] == '\0' || s2[i1] == '\0'))
 		i2 = (unsigned char)s1[i1] - (unsigned char)s2[i1];
 	return (i2);
 }
